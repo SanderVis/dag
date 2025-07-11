f8103910ecd7c0402efa2cb4e7ea18e28d974266
SanderVis <sandervissers61@hotmail.com>
2025-07-11 15:16:13 +0200

    Update Sidebar.js

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,1 +1,13 @@
+export default function Sidebar() {
+  return (
+    <div style={{ width: '250px', backgroundColor: '#f0f0f0', padding: '1rem' }}>
+      <h3>Folders</h3>
+      <ul>
+        <li>Infectieziekten</li>
+        <li>Immuundeficiënties</li>
+        <li>Algemene interne geneeskunde</li>
+      </ul>
+    </div>
+  )
+}
 // Sidebar component
