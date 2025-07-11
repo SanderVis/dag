37781c2870b8c6da9bd4a11b8601a139284162cb
SanderVis <sandervissers61@hotmail.com>
2025-07-11 15:16:32 +0200

    Update Editor.js

--- components/Editor.js
+++ components/Editor.js
@@ -1,1 +1,8 @@
-// Editor component placeholder
+export default function Editor() {
+  return (
+    <div style={{ marginTop: '1rem' }}>
+      <h2>Editor</h2>
+      <textarea style={{ width: '100%', height: '300px' }} placeholder="Type hier je notities..." />
+    </div>
+  )
+}
